fe43e94a6c3895fda30fbb7e1fd9ce3cac5a29a5
Arda Coşkun <m.ardacoskun@gmail.com>
2024-03-23 23:19:27 +0300

    get image data from post body and use with next image

--- components/Post/PostBody/index.tsx
+++ components/Post/PostBody/index.tsx
@@ -1,7 +1,27 @@
-import parse from "html-react-parser";
+import Image from "next/image";
+import parse, { Element } from "html-react-parser";
 
 const PostBody = ({ body }: { body: string }) => {
-  return <div className="rich-text">{parse(body)}</div>;
+  const options = {
+    replace: (domNode: any) => {
+      if (domNode instanceof Element && domNode.attribs) {
+        if (domNode.name === "img") {
+          const { src, alt } = domNode.attribs;
+          return (
+            <Image
+              className="object-cover object-center w-full h-auto my-3 rounded-md max-h-[300px] md:max-h-[500px]"
+              src={src}
+              alt={alt}
+              width={1280}
+              height={620}
+            />
+          );
+        }
+      }
+    },
+  };
+
+  return <div className="rich-text">{parse(body, options)}</div>;
 };
 
 export default PostBody;
