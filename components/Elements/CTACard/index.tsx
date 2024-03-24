843a1669323204de565edfcf255bcf009b8c88f1
Arda Coşkun <m.ardacoskun@gmail.com>
2024-03-24 11:36:52 +0300

    server actions connedted with cta card

--- components/Elements/CTACard/index.tsx
+++ components/Elements/CTACard/index.tsx
@@ -1,10 +1,24 @@
+/* eslint-disable react/no-unescaped-entities */
+import directus from "@/lib/directus";
 import Image from "next/image";
 
-/* eslint-disable react/no-unescaped-entities */
-const CTACard = () => {
+const CTACard = async () => {
+  const formAction = async (formData: FormData) => {
+    "use server";
+    try {
+      const email = formData.get("email");
+      await directus.items("subscribers").createOne({
+        email,
+      });
+    } catch (error) {
+      console.log("error");
+      throw new Error("Error create subscriber!");
+    }
+  };
+
   return (
-    <div className="rounded-md bg-slate-100 py-10 px-6 relative overflow-hidden">
-      <div className="absolute inset-0 bg-gradient-to-br from-white/95 via-white/70 to-white/30 z-10" />
+    <div className="relative px-6 py-10 overflow-hidden rounded-md bg-slate-100">
+      <div className="absolute inset-0 z-10 bg-gradient-to-br from-white/95 via-white/70 to-white/30" />
       <Image
         src="https://images.unsplash.com/photo-1672600830594-ae4ccc159578?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1263&q=80"
         alt="CTA Card Image"
@@ -12,21 +26,26 @@ const CTACard = () => {
         className="object-cover object-center"
       />
       <div className="relative z-20">
-        <div className="font-medium text-lg">#exploretheworld</div>
+        <div className="text-lg font-medium">#exploretheworld</div>
         <h3 className="mt-3 text-4xl font-semibold">
           Explore the world with me!
         </h3>
-        <p className="mt-2 text-lg max-w-lg">
+        <p className="max-w-lg mt-2 text-lg">
           Explore the world with me! I'm travelling around the 🌍. I've visited
           most of the great cities of 🇺🇸 and currently I'm travelling in 🇪🇺 Join
           me!
         </p>
-        <form className="mt-6 flex items-center gap-2 w-full">
+        <form
+          action={formAction}
+          className="flex items-center w-full gap-2 mt-6"
+        >
           <input
-            className="bg-white/80 text-base rounded-md py-2 px-3 outline-none focus:ring-2 ring-neutral-600 placeholder:text-sm w-full md:w-auto"
+            type="email"
+            name="email"
+            className="w-full px-3 py-2 text-base rounded-md outline-none bg-white/80 focus:ring-2 ring-neutral-600 placeholder:text-sm md:w-auto"
             placeholder="Write your email"
           />
-          <button className="bg-neutral-900 rounded-md px-3 py-2 text-neutral-200 hover:bg-neutral-500 transition-all whitespace-nowrap">
+          <button className="px-3 py-2 transition-all rounded-md bg-neutral-900 text-neutral-200 hover:bg-neutral-500 whitespace-nowrap">
             Sign Up
           </button>
         </form>
